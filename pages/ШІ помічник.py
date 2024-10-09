2b32cbe372c44e8acb3cc115b928658b6b974277
Vlonedbvmn <121385436+Vlonedbvmn@users.noreply.github.com>
2024-10-09 15:51:47 +0300

    Update ШІ помічник.py

--- pages/ШІ помічник.py
+++ pages/ШІ помічник.py
@@ -1447,7 +1447,7 @@ if __name__ == "__main__":
             st.session_state.date_not_n = True
             ds_for_pred['ds'] = [i for i in range(1, len(ds_for_pred) + 1)]
         st.title("ШІ помічник")
-        st.markdown(f"## Зараз працюю з обраним Вами набором даних: {st.session_state.name}")
+        st.markdown(f"### Зараз ШІ помічник працює з набором даних: {st.session_state.name}")
         st.write(" ")
         st.markdown("## Приклади запитів до ШІ помічника:")
         for message in st.session_state.messages1:
