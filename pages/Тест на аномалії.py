7953d26f3959ff9a0648efcb3d9215d564022590
Vlonedbvmn <121385436+Vlonedbvmn@users.noreply.github.com>
2024-10-16 14:05:06 +0300

    Update Тест на аномалії.py

--- pages/Тест на аномалії.py
+++ pages/Тест на аномалії.py
@@ -197,7 +197,7 @@ if __name__ == "__main__":
         
             # Show the plot
             st.session_state.fig_a = fig
-                    st.plotly_chart(st.session_state.fig_a, use_container_width=True)
+            st.plotly_chart(st.session_state.fig_a, use_container_width=True)
 
     else:
         st.warning('Для проведення тесту на аномалії, оберіть дані', icon="⚠️")
