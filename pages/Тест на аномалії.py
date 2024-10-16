6ca08d7ab4bd70b7567a1e6a0a7435fc2a914eb9
Vlonedbvmn <121385436+Vlonedbvmn@users.noreply.github.com>
2024-10-16 14:48:43 +0300

    Update Тест на аномалії.py

--- pages/Тест на аномалії.py
+++ pages/Тест на аномалії.py
@@ -209,11 +209,11 @@ if __name__ == "__main__":
         
             # Highlight anomalies
             
-            anomalie_count = 0
-            for i in datafra[:sl]['anomaly'].values.tolist():
-                if i:
-                    anomalie_count += 1
-            print(anomalie_count)
+            # anomalie_count = 0
+            # for i in datafra[:sl]['anomaly'].values.tolist():
+            #     if i:
+            #         anomalie_count += 1
+            # print(anomalie_count)
             anomalies = datafra[:sl][datafra['anomaly'] == True]
             fig.add_trace(go.Scatter(x=anomalies['ds'], y=anomalies['y'], mode='markers', name='Аномалія',
                                      marker=dict(color='red', size=8)))
